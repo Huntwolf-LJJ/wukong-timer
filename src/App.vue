df1a9d27612508cf1fb3e9c62edf286e3ae7c355
huntwolf <1063700371@qq.com>
2024-08-08 19:03:47 +0800

    feat: 适配移动端

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,63 @@
 <template>
   <div class="main">
-    <video :src="themeVideo" autoplay muted loop preload="auto" playsinline class="theme-video"/>
+    <video v-if="!isPhone" :src="themeVideo" autoplay muted loop preload="auto" playsinline class="theme-video"/>
 
-    <div class="subtitle-pic">
+    <div v-else class="theme-bg">
+      <img :src="themeBg"/>
+    </div>
+
+    <div :class="isPhone ? 'subtitle-pic-phone' : 'subtitle-pic'">
       <img :src="homeSubTitle">
     </div>
-    <div class="home-wrapper">
-      <div class="home-logo">
-        <img :src="homeLogo">
-      </div>
 
-      <div class="countdown" v-if="isShowCountdown">
-        <VueCountdown :time="countdownTime" :interval="100" v-slot="{days, hours, minutes, seconds, milliseconds}" @end="handleEndOfCountdown">
-          还剩：{{ days }} 天 {{ hours }} 小时 {{ minutes }} 分钟 {{ seconds }}.{{ Math.floor(milliseconds / 100) }} 秒
-        </VueCountdown>
-      </div>
+    <div :class="isPhone ? 'home-wrapper-phone' : 'home-wrapper'">
+      <template v-if="!isPhone">
+        <div class="home-logo">
+          <img :src="homeLogo">
+        </div>
 
-      <div class="to-play-btn" v-else @click="toPlay">
-        现在去玩
-      </div>
+        <div class="countdown" v-if="isShowCountdown">
+          <VueCountdown :time="countdownTime" :interval="100" v-slot="{days, hours, minutes, seconds, milliseconds}" @end="handleEndOfCountdown">
+            还剩：{{ days }} 天 {{ hours }} 小时 {{ minutes }} 分钟 {{ seconds }}.{{ Math.floor(milliseconds / 100) }} 秒
+          </VueCountdown>
+        </div>
+
+        <div class="to-play-btn" v-else-if="!isPhone" @click="toPlay">
+          现在去玩
+        </div>
+      </template>
+
+      <template v-else>
+        <div class="home-logo-phone">
+          <img :src="homeLogoPhone"/>
+        </div>
+
+        <div class="home-time-phone">
+          <img :src="timePhone">
+        </div>
+
+        <div class="countdown-phone">
+          <VueCountdown :time="countdownTime" :interval="100" v-slot="{days, hours, minutes, seconds, milliseconds}" @end="handleEndOfCountdown">
+            还剩：{{ days }} 天 {{ hours }} 小时 {{ minutes }} 分钟 {{ seconds }}.{{ Math.floor(milliseconds / 100) }} 秒
+          </VueCountdown>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script setup>
 import themeVideo from '@/assets/videos/theme-video.mp4'
+import themeBg from '@/assets/images/theme-bg.png'
 import homeLogo from '@/assets/images/home-logo.png'
+import homeLogoPhone from '@/assets/images/phone-home-logo.png'
+import timePhone from '@/assets/images/phone-time.png'
 import homeSubTitle from '@/assets/images/home-subtitle.png'
 import VueCountdown from '@chenfengyuan/vue-countdown';
 import {computed, onMounted, ref} from "vue";
 
 const isShowCountdown = ref(true)
+const isPhone = ref(false)
 const now = new Date()
 const playTime = new Date(2024, 7, 20, 0, 0, 0, 0)
 const countdownTime = computed(() => {
@@ -46,6 +73,11 @@ const toPlay = () => {
 }
 
 onMounted(() => {
+  window.onresize = () => {
+    isPhone.value = window.matchMedia("(max-width: 900px)").matches;
+  }
+  isPhone.value = window.matchMedia("(max-width: 900px)").matches;
+
   if (now.getTime() >= playTime.getTime()) {
     handleEndOfCountdown()
   }
@@ -58,10 +90,17 @@ onMounted(() => {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  background: #000;
 }
 
 .theme-video {
   width: 100%;
+}
+
+.theme-bg {
+  position: absolute;
+  width: 100%;
+  //height: 100%;
 }
 
 .home-wrapper {
@@ -73,23 +112,58 @@ onMounted(() => {
   align-items: center;
 }
 
+.home-wrapper-phone {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 .home-logo {
   width: 626px;
   height: 454px;
   margin-bottom: 80px;
 }
 
+.home-logo-phone {
+  width: 980px;
+  //height: 454px;
+  margin-top: 400px;
+}
+
+.home-time-phone {
+  margin-top: 880px;
+  width: 70%;
+}
+
 .subtitle-pic {
   position: absolute;
   width: 34px;
-  height: 93px;
   top: 145.94px;
   right: 130px;
+}
+
+.subtitle-pic-phone {
+  position: absolute;
+  width: 114px;
+  top: 310px;
+  left: 100px;
 }
 
 .countdown {
   color: white;
   font-size: 32px;
+  text-align: center;
+}
+
+.countdown-phone {
+  color: white;
+  //width: 70%;
+  font-size: 88px;
+  text-align: center;
+  margin-top: 90px;
 }
 
 .to-play-btn {
@@ -104,9 +178,5 @@ onMounted(() => {
   border-radius: 1px;
   font-size: 30px;
   cursor: pointer;
-}
-
-@media only screen and (min-width: 900px){
-
 }
 </style>
